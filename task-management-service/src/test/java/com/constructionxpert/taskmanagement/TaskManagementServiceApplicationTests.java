d370a79a51da2aab2cd463a7795c0e7d3552b553
Nabil Sabi <nabilsabi01@gmail.com>
2024-08-28 00:06:07 +0100

    Créer le projet Spring Boot pour le service de gestion des tâches.

---
+++ task-management-service/src/test/java/com/constructionxpert/taskmanagement/TaskManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constructionxpert.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
